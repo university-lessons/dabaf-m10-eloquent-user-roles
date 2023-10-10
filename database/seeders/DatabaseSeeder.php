23becf9912d18b74b28ee43dabd0272ff405f926
Andres <andres.porfirio.tablet@gmail.com>
2023-10-10 18:01:58 -0300

    lesson code

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Role;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +20,9 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Role::create(['name' => 'Teacher']);
+        Role::create(['name' => 'Student']);
+        Role::create(['name' => 'Monitor']);
     }
 }
